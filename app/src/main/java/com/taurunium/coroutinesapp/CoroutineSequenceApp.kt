aa73a10172e61e9893bc127a66e6d0411f2e00a1
DESKTOP-N4VRD92\Fujitsu <vidojemuric@gmail.com>
2023-05-24 15:41:57 +0300

    Parallel coroutines

--- app/src/main/java/com/taurunium/coroutinesapp/CoroutineSequenceApp.kt
+++ app/src/main/java/com/taurunium/coroutinesapp/CoroutineSequenceApp.kt
@@ -3,10 +3,7 @@ package com.taurunium.coroutinesapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 class CoroutineSequenceApp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,21 +11,23 @@ class CoroutineSequenceApp : AppCompatActivity() {
         setContentView(R.layout.activity_coroutine_sequence_app)
 
         CoroutineScope(Dispatchers.IO).launch {
-            val one = doSomethingUseful1()
-            val two = doSomethingUseful2()
+            Log.d("TAGY", "app started: ")
+            val one = async { doSomethingUseful1() }
+            val two = async { doSomethingUseful2() }
 
-            val result = one + two
-            Log.d("TAGY", "onCreate: "+result)
+            val result = one.await() + two.await()
+            Log.d("TAGY", "onCreate: " + result)
         }
 
     }
-    suspend fun doSomethingUseful1(): Int{
+
+    suspend fun doSomethingUseful1(): Int {
         delay(5000)
         Log.d("TAGY", "doSomethingUseful1 done: ")
         return 11
     }
 
-    suspend fun doSomethingUseful2(): Int{
+    suspend fun doSomethingUseful2(): Int {
         delay(7000)
         Log.d("TAGY", "doSomethingUseful2 done: ")
         return 8
